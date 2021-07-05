aba3bcc20a11fcd4393a67c7907b0f4962779cb7
Mahdi AlKhalaf <m@mahdi.pro>
2021-07-05 21:12:18 +0300

    feat: Add initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Box, MenuItem, Select, TextField, Typography} from "@material-ui/core/index";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+export default function App() {
+    const lineFormats = ["1h30m4s"]
+    const operations = ["Sum"]
+
+    const [input, setInput] = useState("")
+    const [selectedFormat, setSelectedFormat] = useState(lineFormats[0])
+    const [selectedOperation, setSelectedOperation] = useState(operations[0])
+
+    const regex = /^((?<hours>\d+)h)?((?<minutes>\d+)m)?((?<seconds>\d+)m)?$/g;
+
+    const [resultHours, resultMinutes, resultSeconds] = input.split("\n").map(line => {
+        const match = regex.exec(line);
+        const hours = parseInt(match?.groups?.hours || "0");
+        const minutes = parseInt(match?.groups?.minutes || "0");
+        const seconds = parseInt(match?.groups?.seconds || "0");
+        return [hours, minutes, seconds]
+    }).reduce((previousValue, currentValue) => {
+        const [prevHours, prevMinutes, prevSeconds] = previousValue
+        const [currHours, currMinutes, currSeconds] = currentValue
+        const totalSeconds = prevHours * 3600 + currHours * 3600 + prevMinutes * 60 + currMinutes * 60 + prevSeconds + currSeconds
+        const resultHours = Math.floor(totalSeconds / 3600)
+        const resultMinutes = Math.floor(totalSeconds / 60 % 60)
+        const resultSeconds = Math.floor(totalSeconds % 60)
+        return [resultHours, resultMinutes, resultSeconds]
+    }, [0, 0, 0])
+
+    return <Box p={"16px"}>
+        <Typography variant={"h2"}>Time Calculator</Typography>
+        <Box
+            display={"grid"}
+            gridTemplateColumns={"auto"}
+            gridGap={"16px"}
+            p={"32px"}
+        >
+            <Typography variant={"h4"}>Input</Typography>
+            <TextField
+                variant={"filled"}
+                multiline={true}
+                rows={20}
+                value={input}
+                onChange={event => setInput(event.target.value as string)}
+            />
+            <Typography variant={"h4"}>Line Format</Typography>
+            <Select
+                variant={"filled"}
+                value={selectedFormat}
+                onChange={event => setSelectedFormat(event.target.value as string)}
+            >
+                {lineFormats.map(format =>
+                    <MenuItem key={format} value={format}>{format}</MenuItem>
+                )}
+            </Select>
+            <Typography variant={"h4"}>Operation</Typography>
+            <Select
+                variant={"filled"}
+                value={selectedOperation}
+                onChange={event => setSelectedOperation(event.target.value as string)}
+            >
+                {operations.map(operation =>
+                    <MenuItem key={operation} value={operation}>{operation}</MenuItem>
+                )}
+            </Select>
+            <Typography variant={"h4"}>Result</Typography>
+            <Box
+                display={"grid"}
+                gridTemplateColumns={"auto auto 1fr"}
+                gridGap={"8px"}
+                alignItems={"center"}
+            >
+                <Typography variant={"h6"}>Hours</Typography>
+                <TextField
+                    variant={"filled"}
+                    value={resultHours}
+                />
+                <Box/>
+                <Typography variant={"h6"}>Minutes</Typography>
+                <TextField
+                    variant={"filled"}
+                    value={resultMinutes}
+                />
+                <Box/>
+                <Typography variant={"h6"}>Seconds</Typography>
+                <TextField
+                    variant={"filled"}
+                    value={resultSeconds}
+                />
+                <Box/>
+            </Box>
+        </Box>
+    </Box>
 }
-
-export default App;
